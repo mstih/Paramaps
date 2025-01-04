196fdd85b1822bb67552d65933e0da2c4584fb42
Miha <mstih@proton.me>
2025-01-04 18:44:52 +0100

    Initial Commit

---
+++ mapstest/mapstestApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapstestApp.swift
+//  mapstest
+//
+//  Created by Miha Štih on 4. 1. 25.
+//
+
+import SwiftUI
+
+@main
+struct mapstestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mapstestTests/mapstestTests.swift
@@ -0,0 +1,17 @@
+//
+//  mapstestTests.swift
+//  mapstestTests
+//
+//  Created by Miha Štih on 4. 1. 25.
+//
+
+import Testing
+@testable import mapstest
+
+struct mapstestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
